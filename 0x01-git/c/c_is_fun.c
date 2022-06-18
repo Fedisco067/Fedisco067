39342e646bc6ae245e406032e31d83cf19809973
Njoku Chinagorom <fedisco067@gmail.com>
2022-06-17 18:49:01 -0700

    starting to code today,so cool

---
+++ 0x01-git/c/c_is_fun.c
@@ -0,0 +1,17 @@
+ls
+cat> js/main.js
+cat> js/index.js
+cat> bash/alx
+#!/bin/bas
+echo "ALX"
+cat> bash/school 
+#!bin/bash
+echo "School"
+git add .
+commit -m c/c_is_fun.c
+commit -m js/main.js
+commit -m js/index.js
+commit -m bash/alx
+commit -m bash/school 
+push
+
---
+++ 0x01-git/c/main.js
@@ -0,0 +1,5 @@
+cat > index.js
+cd bash
+ls
+cd 0x01-git
+clear 
